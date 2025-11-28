d1f1480ffd91965248e2442f299bc04a2b09efd0
Indra Hertanto <indra.hertanto@gmail.com>
2025-11-28 23:02:06 +0700

    fix error Column "CATEGORY" not found

--- src/main/java/com/ubl/productapi/model/Product.java
+++ src/main/java/com/ubl/productapi/model/Product.java
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 	    name = "products",
 	    indexes = {
 	        @Index(name = "idx_product_name", columnList = "name"),
-	        @Index(name = "idx_product_category", columnList = "category"),
+	        @Index(name = "idx_product_category", columnList = "category_id"),
 	        @Index(name = "idx_product_price", columnList = "price")
 	    }
 	)
